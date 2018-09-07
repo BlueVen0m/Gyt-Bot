3d218e37cc93c9c075b858a62cec151362e3783d
BlueVen0m <43078827+BlueVen0m@users.noreply.github.com>
2018-09-07 23:47:22 +0200

    Add files via upload

---
+++ .gitignore/main.js
@@ -0,0 +1,4 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+//CLIENT TOKEN NDg1MzYyMDkyODY2Nzk3NTY4.DnL7Rw.owDCnDexdkwky9qDHvMAt7j8vxk
+bot.login("NDg1MzYyMDkyODY2Nzk3NTY4.DnL7Rw.owDCnDexdkwky9qDHvMAt7j8vxk");
