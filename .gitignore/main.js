de116eb82ab8d7fbfffe94d4f6b157539443cb88
BlueVen0m <43078827+BlueVen0m@users.noreply.github.com>
2018-09-08 18:45:14 +0200

    Delete main.js

--- .gitignore/main.js
+++
@@ -1,4 +0,0 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-//CLIENT TOKEN NDg1MzYyMDkyODY2Nzk3NTY4.DnL7Rw.owDCnDexdkwky9qDHvMAt7j8vxk
-bot.login("NDg1MzYyMDkyODY2Nzk3NTY4.DnL7Rw.owDCnDexdkwky9qDHvMAt7j8vxk");
